10937ee284a7115e411590709c232930e2304d82
Smail <44433524+Smail@users.noreply.github.com>
2023-03-25 01:46:23 +0100

    Style: Move constructors to the top of the file

--- f1udpinterface/packets/PacketCarDamageData.cpp
+++ f1udpinterface/packets/PacketCarDamageData.cpp
@@ -2,6 +2,36 @@
 #include "../../util.h"
 
 namespace F122::Network {
+    PacketCarDamageData::Data::Data(const std::array<std::uint8_t, 42>& bytes) :
+            m_tyresWear{util::convert<float>({bytes[0], bytes[1], bytes[2], bytes[3]}),
+                        util::convert<float>({bytes[4], bytes[5], bytes[6], bytes[7]}),
+                        util::convert<float>({bytes[8], bytes[9], bytes[10], bytes[11]}),
+                        util::convert<float>({bytes[12], bytes[13], bytes[14], bytes[15]})},
+            m_tyresDamage{bytes[16], bytes[17], bytes[18], bytes[19]},
+            m_brakesDamage{bytes[20], bytes[21], bytes[22], bytes[23]},
+            m_frontLeftWingDamage{bytes[24]},
+            m_frontRightWingDamage{bytes[25]},
+            m_rearWingDamage{bytes[26]},
+            m_floorDamage{bytes[27]},
+            m_diffuserDamage{bytes[28]},
+            m_sidepodDamage{bytes[29]},
+            m_drsFault{bytes[30]},
+            m_ersFault{bytes[31]},
+            m_gearBoxDamage{bytes[32]},
+            m_engineDamage{bytes[33]},
+            m_engineMGUHWear{bytes[34]},
+            m_engineESWear{bytes[35]},
+            m_engineCEWear{bytes[36]},
+            m_engineICEWear{bytes[37]},
+            m_engineMGUKWear{bytes[38]},
+            m_engineTCWear{bytes[39]},
+            m_engineBlown{bytes[40]},
+            m_engineSeized{bytes[41]} {}
+
+    PacketCarDamageData::PacketCarDamageData(const std::array<std::uint8_t, 948>& bytes) :
+            m_header{{util::copy_resize<std::uint8_t, 948, 24>(bytes)}},
+            m_carDamageData{util::batch_create<Data, 948, 42, 22, 24>(bytes)} {}
+
     std::ostream& operator<<(std::ostream& os, const PacketCarDamageData& data) {
         os << "m_header: " << "\n" << data.m_header << "\n"
            << "m_carDamageData: " << "\n\n";
@@ -39,34 +69,4 @@ namespace F122::Network {
 
         return os;
     }
-
-    PacketCarDamageData::Data::Data(const std::array<std::uint8_t, 42>& bytes) :
-            m_tyresWear{util::convert<float>({bytes[0], bytes[1], bytes[2], bytes[3]}),
-                        util::convert<float>({bytes[4], bytes[5], bytes[6], bytes[7]}),
-                        util::convert<float>({bytes[8], bytes[9], bytes[10], bytes[11]}),
-                        util::convert<float>({bytes[12], bytes[13], bytes[14], bytes[15]})},
-            m_tyresDamage{bytes[16], bytes[17], bytes[18], bytes[19]},
-            m_brakesDamage{bytes[20], bytes[21], bytes[22], bytes[23]},
-            m_frontLeftWingDamage{bytes[24]},
-            m_frontRightWingDamage{bytes[25]},
-            m_rearWingDamage{bytes[26]},
-            m_floorDamage{bytes[27]},
-            m_diffuserDamage{bytes[28]},
-            m_sidepodDamage{bytes[29]},
-            m_drsFault{bytes[30]},
-            m_ersFault{bytes[31]},
-            m_gearBoxDamage{bytes[32]},
-            m_engineDamage{bytes[33]},
-            m_engineMGUHWear{bytes[34]},
-            m_engineESWear{bytes[35]},
-            m_engineCEWear{bytes[36]},
-            m_engineICEWear{bytes[37]},
-            m_engineMGUKWear{bytes[38]},
-            m_engineTCWear{bytes[39]},
-            m_engineBlown{bytes[40]},
-            m_engineSeized{bytes[41]} {}
-
-    PacketCarDamageData::PacketCarDamageData(const std::array<std::uint8_t, 948>& bytes) :
-            m_header{{util::copy_resize<std::uint8_t, 948, 24>(bytes)}},
-            m_carDamageData{util::batch_create<Data, 948, 42, 22, 24>(bytes)} {}
 }
