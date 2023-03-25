9a92ed2d32a94289522e7a1ca1a69c6260d8e93c
Smail <44433524+Smail@users.noreply.github.com>
2023-03-25 08:13:27 +0100

    (Re-)Implement UDP readout of F1 game data.

--- main.cpp
+++ main.cpp
@@ -13,43 +13,100 @@
 #include "f1udpinterface/public.h"
 
 using namespace F122::Network::Packets;
+using PacketVariant = std::variant<CarDamageData*, CarSetupData*, CarStatusData*, CarTelemetryData*, EventData*,
+        FinalClassificationData*, LapData*, LobbyInfoData*, MotionData*, ParticipantsData*, SessionData*, SessionHistoryData*>;
 
-//Packet createPacket(const QByteArray& bytes) {
-//    Packet packet;
-//
-//
-//
-//    return packet;
-//}
+template<size_t N>
+std::array<std::uint8_t, N> copy_to_array(const QByteArray& bytes) {
+    std::array<std::uint8_t, N> array{};
+    std::copy(bytes.begin(), bytes.end(), array.begin());
 
-void processTheDatagram(const QNetworkDatagram& datagram, std::ofstream& file, QLineSeries* series) {
-    QByteArray bytes = datagram.data();
+    return array;
+}
 
-    std::array<std::uint8_t, 24> packetHeaderBytes{};
+/// A factory to create the specific data structure from the incoming network packet.
+PacketVariant createPacket(const QByteArray& bytes) {
+    std::variant<CarDamageData*, CarSetupData*, CarStatusData*, CarTelemetryData*, EventData*, FinalClassificationData*,
+            LapData*, LobbyInfoData*, MotionData*, ParticipantsData*, SessionData*, SessionHistoryData*> packetVariant{};
 
-    CarDamageData packet{std::array<std::uint8_t, CarDamageData::SIZE>{}};
-
-    size_t size = packet.expectedPacketSize;
-
-    std::cout << (*reinterpret_cast<PacketHeader*>(bytes.data())) << std::endl;
-
-    for (int i = 0; i < 24; ++i) {
-        packetHeaderBytes[i] = bytes[i];
+    switch (bytes.size()) {
+        case CarDamageData::SIZE:
+            packetVariant = new CarDamageData{copy_to_array<CarDamageData::SIZE>(bytes)};
+            break;
+        case CarSetupData::SIZE:
+            packetVariant = new CarSetupData{copy_to_array<CarSetupData::SIZE>(bytes)};
+            break;
+        case CarStatusData::SIZE:
+            packetVariant = new CarStatusData{copy_to_array<CarStatusData::SIZE>(bytes)};
+            break;
+        case CarTelemetryData::SIZE:
+            packetVariant = new CarTelemetryData{copy_to_array<CarTelemetryData::SIZE>(bytes)};
+            break;
+        case EventData::SIZE:
+            packetVariant = new EventData{copy_to_array<EventData::SIZE>(bytes)};
+            break;
+        case FinalClassificationData::SIZE:
+            packetVariant = new FinalClassificationData{copy_to_array<FinalClassificationData::SIZE>(bytes)};
+            break;
+        case LapData::SIZE:
+            packetVariant = new LapData{copy_to_array<LapData::SIZE>(bytes)};
+            break;
+        case LobbyInfoData::SIZE:
+            packetVariant = new LobbyInfoData{copy_to_array<LobbyInfoData::SIZE>(bytes)};
+            break;
+        case MotionData::SIZE:
+            packetVariant = new MotionData{copy_to_array<MotionData::SIZE>(bytes)};
+            break;
+        case ParticipantsData::SIZE:
+            packetVariant = new ParticipantsData{copy_to_array<ParticipantsData::SIZE>(bytes)};
+            break;
+        case SessionData::SIZE:
+            packetVariant = new SessionData{copy_to_array<SessionData::SIZE>(bytes)};
+            break;
+        case SessionHistoryData::SIZE:
+            packetVariant = new SessionHistoryData{copy_to_array<SessionHistoryData::SIZE>(bytes)};
+            break;
+        default:
+            throw std::invalid_argument(
+                    "Unknown byte size: The given byte array doesn't match any known type in this factory");
     }
 
-//    chart->update();
+    return packetVariant;
 
-    std::cout << (*reinterpret_cast<PacketHeader*>(packetHeaderBytes.data())) << std::endl;
-    std::cout << "c: " << (PacketHeader{packetHeaderBytes}) << std::endl;
+//    switch (bytes.size()) {
+//        case CarDamageData::SIZE:
+//            return new CarDamageData{copy_to_array<CarDamageData::SIZE>(bytes)};
+//        case CarSetupData::SIZE:
+//            return new CarSetupData{copy_to_array<CarSetupData::SIZE>(bytes)};
+//        case CarStatusData::SIZE:
+//            return new CarStatusData{copy_to_array<CarStatusData::SIZE>(bytes)};
+//        case CarTelemetryData::SIZE:
+//            return new CarTelemetryData{copy_to_array<CarTelemetryData::SIZE>(bytes)};
+//        case EventData::SIZE:
+//            return new EventData{copy_to_array<EventData::SIZE>(bytes)};
+//        case FinalClassificationData::SIZE:
+//            return new FinalClassificationData{copy_to_array<FinalClassificationData::SIZE>(bytes)};
+//        case LapData::SIZE:
+//            return new LapData{copy_to_array<LapData::SIZE>(bytes)};
+//        case LobbyInfoData::SIZE:
+//            return new LobbyInfoData{copy_to_array<LobbyInfoData::SIZE>(bytes)};
+//        case MotionData::SIZE:
+//            return new MotionData{copy_to_array<MotionData::SIZE>(bytes)};
+//        case ParticipantsData::SIZE:
+//            return new ParticipantsData{copy_to_array<ParticipantsData::SIZE>(bytes)};
+//        case SessionData::SIZE:
+//            return new SessionData{copy_to_array<SessionData::SIZE>(bytes)};
+//        case SessionHistoryData::SIZE:
+//            return new SessionHistoryData{copy_to_array<SessionHistoryData::SIZE>(bytes)};
+//        default:
+//            throw std::invalid_argument(
+//                    "Unknown byte size: The given byte array doesn't match any known type in this factory");
+//    }
+}
 
-    series->append(7, 4);
-
-//    Packet* packet = PacketFactory::create_from_bytes(bytes);
-
-//    std::cout << (*packet) << std::endl;
-
-//    std::visit([](const auto &x) { std::cout << (*x) << std::endl; }, packetVariant);
-//    std::visit([&file](const auto &x) { file << (*x) << std::endl; }, packetVariant);
+void processTheDatagram(const QNetworkDatagram& datagram, std::ofstream& file, QLineSeries* series) {
+    if (datagram.data().size() == EventData::SIZE)
+        std::visit([](const auto& x) { std::cout << (*x) << std::endl; }, createPacket(datagram.data()));
 }
 
 int main(int argc, char* argv[]) {
@@ -60,7 +117,7 @@ int main(int argc, char* argv[]) {
     auto* series = new QLineSeries();
 
     udpSocket.bind(20777, QUdpSocket::ShareAddress);
-    QObject::connect(&udpSocket, &QUdpSocket::readyRead, [&udpSocket, &series, &chart] () {
+    QObject::connect(&udpSocket, &QUdpSocket::readyRead, [&udpSocket, &series, &chart]() {
         std::ofstream file{"output.txt", std::ios::trunc};
 
         while (udpSocket.hasPendingDatagrams()) {
