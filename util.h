a9c1914bfd8ae04d709de050ece7b5216b5bb120
Smail <44433524+Smail@users.noreply.github.com>
2023-03-25 01:25:54 +0100

    Fix linker error

--- util.h
+++ util.h
@@ -6,12 +6,6 @@
 #include <array>
 
 namespace util {
-    template<class T, size_t N>
-    std::string to_string(const std::array<T, N> &array, std::function<std::string(T)> to_string);
-
-    template<class T, size_t N>
-    std::string to_string(const std::array<T, N> &array);
-
     template<typename T>
     constexpr T convert(const std::array<std::uint8_t, sizeof(T)>& bytes) {
         T result{0};
@@ -29,13 +23,55 @@ namespace util {
         return std::bit_cast<double>(convert<std::uint64_t>(bytes));
     }
 
+    template<class T, size_t N>
+    std::string to_string(const std::array<T, N>& array, std::function<std::string(T)> to_string) {
+        std::string result = "[";
+
+        for (size_t i = 0; i < N - 1; ++i) {
+            result += to_string(array[i]) + ", ";
+        }
+        result += to_string(array[N - 1]) + "]";
+
+        return result;
+    }
+
+    template<class T, size_t N>
+    std::string to_string(const std::array<T, N>& array) {
+        std::string result = "[";
+
+        for (size_t i = 0; i < N - 1; ++i) {
+            result += std::to_string(array[i]) + ", ";
+        }
+        result += std::to_string(array[N - 1]) + "]";
+
+        return result;
+    }
+
     template<typename T, size_t CURRENT_SIZE, size_t NEW_SIZE, size_t FROM = 0>
-    std::array<T, NEW_SIZE> copy_resize(const std::array<T, CURRENT_SIZE>& array);
+    std::array<T, NEW_SIZE> copy_resize(const std::array<T, CURRENT_SIZE>& array) {
+        std::array<T, NEW_SIZE> result{};
+
+        std::copy_n(array.cbegin() + FROM, NEW_SIZE, result.begin());
+
+        return result;
+    }
 
     template<size_t C>
-    std::array<char, C> convert_type(const std::array<std::uint8_t, C>& array);
-
+    std::array<char, C> convert_type(const std::array<std::uint8_t, C>& array) {
+        return (const std::array<char, 48>&) array;
+    }
 
     template<typename T, size_t PACKET_SIZE, size_t DATA_SIZE, size_t RESULT_SIZE, size_t OFFSET>
-    std::array<T, RESULT_SIZE> batch_create(const std::array<std::uint8_t, PACKET_SIZE>& bytes);
+    std::array<T, RESULT_SIZE> batch_create(const std::array<std::uint8_t, PACKET_SIZE>& bytes) {
+        std::array<T, RESULT_SIZE> result{};
+
+        for (size_t i = 0; i < RESULT_SIZE; ++i) {
+            std::array<std::uint8_t, DATA_SIZE> xs{};
+            std::copy_n(bytes.cbegin() + i * DATA_SIZE + OFFSET, DATA_SIZE, xs.begin());
+            // result[i] = T{util::copy_resize<std::uint8_t, PACKET_SIZE, DATA_SIZE, i * DATA_SIZE + OFFSET>(bytes)};
+            result[i] = T{xs};
+        }
+
+        return result;
+    }
 }
