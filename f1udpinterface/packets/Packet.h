02f5550dc449fd547cd8f2d50cc67f9a5bb13f33
Smail <44433524+Smail@users.noreply.github.com>
2023-03-29 05:47:01 +0200

    Remove unused expectedPacketSize

--- f1udpinterface/packets/Packet.h
+++ f1udpinterface/packets/Packet.h
@@ -4,16 +4,10 @@
 #include <cstdint>
 
 namespace F122::Network::Packets {
-    class Packet {
-    public:
+    struct Packet {
         template<size_t NETWORK_PACKET_SIZE>
-        explicit constexpr Packet(const std::array<std::uint8_t, NETWORK_PACKET_SIZE>&) :
-                expectedPacketSize{NETWORK_PACKET_SIZE} {}
+        explicit constexpr Packet(const std::array<std::uint8_t, NETWORK_PACKET_SIZE>&) {};
 
         virtual ~Packet() = 0;
-
-    public:
-        /// The size of the network packet this class represents in bytes.
-        const size_t expectedPacketSize;
     };
 }
